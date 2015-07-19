66cd14b67377a5128a18c0d9b4d61d3602eec79a
unknown <chris@chris-Laptop.ad.validationresource.com>
2015-07-19 20:09:34 +0100

    initial commit

---
+++ TestLdapApp/AdsUser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLdapApp
+{
+    class AdsUser
+    {
+        public string ErrorMessage { get; private set; }
+
+        /// <summary>
+        /// Creates a user in an organisational unit(ou) called People within 
+        /// Active Directory. this ou exists inside the root of the domain.
+        /// </summary>
+        /// <returns>true on success or false on failure</returns>
+        public bool CreateAdUser()
+        {
+            DirectoryEntry parent = new DirectoryEntry("LDAP://OU=people,DC=ad.validationresourece.com, DC=com",null,null,AuthenticationTypes.Secure)
+            DirectoryEntry user = parent.Children.Add("CN=test.user","user");
+
+            using(user)
+	        {
+		        //sAMAccountName is required for W2k AD, we would not use this for ADAM, however.
+                user.Properties["sAMAccountName"].Value="test.user";
+                //userPrincipleName is not required, but recommended
+                //For ADAM AD also contains this , so we can use it.
+                user.Properties["userPrincipleName"].Value="test.user";
+                
+                try 
+	            {	        
+		            user.CommitChanges();
+                    
+	            }
+	            catch (Exception ex)
+	            {
+		            ErrorMessage = ex.Message;
+                    return false;
+	            }
+                return true;
+	        }
+        }
+
+        public AdsUserFlags GetUserFlags()
+        {
+            DirectoryEntry user = new DirectoryEntry( "LDAP://CN=User1,CN=users,DC=domain,DC=com", null, null, AuthenticationTypes.Secure ); 
+            AdsUserFlags userFlags = (AdsUserFlags)user.Properties["userAccountControl"].Value;
+            return userFlags;
+        }
+    
+    }
+}
---
+++ TestLdapApp/AdsUserFlags.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLdapApp
+{
+    [Flags]
+    public enum AdsUserFlags
+    {
+        script = 1,                             //0x1
+        AccountDisabled=2,                      //0x2
+        HomeDirectoryRequired=8,                //0x8
+        AccountLockedout=16,                    //0x10
+        PasswordNotRequired=32,                 //0x20
+        PasswordCannotChange=64,                //0x40
+        EncryptedTextPasswordAllowed=128,       //0x80
+        TempDuplicateAccount=256,               //0x100
+        NormalAccount=512,
+        InterDomainTrustAccount=2048,
+        WorkstationTrustAccount=4096,
+        ServerTrustAccount=8192,
+        PasswordDoesNotExpire=65536,
+        MnsLogonAccount=131072,
+        SmartCardRequired=262144,
+        TrustedForDelegation=524288,
+        AccountNotDelegated=1048576,
+        UseDesKeyOnly=1097152,
+        DontRequirePreAuth=4194304,
+        PasswordExpired=8388608,
+        TrustedToAuthenticateForDelegation=16777216,
+        NoAuthDataRequired=33554432
+        
+    }
+}
